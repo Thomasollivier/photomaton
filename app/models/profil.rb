281ce7bde06a173e6164813d15c5a7903ac89fd9
thomas ollivier <thomasollivierfrance@gmail.com>
2017-09-15 12:09:31 +0200

    adding validation on Profil

--- app/models/profil.rb
+++ app/models/profil.rb
@@ -1,2 +1,11 @@
 class Profil < ApplicationRecord
+  validates :name, presence: true;
+  validates :lastname, presence: true;
+  validates :phone, presence: true;
+  validates :mail, presence: true;
+  validates :organisation, presence: true;
+  validates :job, presence: true;
+  validates :expertise, presence: true;
+  validates :interest, presence: true;
+
 end
